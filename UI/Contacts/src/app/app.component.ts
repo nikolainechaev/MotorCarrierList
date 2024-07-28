e7173e4af20dcfed78c6bd617f6025bd00d14aa1
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2024-07-28 06:31:36 -0400

    updated front add to add change contact functionality

--- UI/Contacts/src/app/app.component.ts
+++ UI/Contacts/src/app/app.component.ts
@@ -1,19 +1,18 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs/internal/Observable'
-import { Contact } from '../models/contact.model'
-import { AsyncPipe } from '@angular/common'
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { Observable } from 'rxjs/internal/Observable';
+import { Contact } from '../models/contact.model';
+import { AsyncPipe } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HttpClientModule, AsyncPipe, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
 
   http = inject(HttpClient);
@@ -22,37 +21,64 @@ export class AppComponent {
     email: new FormControl<string | null>(null),
     phone: new FormControl<string>(''),
     favorite: new FormControl<boolean>(false)
-  })
+  });
   contacts$ = this.getContacts();
+  editMode = false;
+  editId: string | null = null;
 
   onFormSubmit() {
-    const addContactRequest = {
+    const contactRequest = {
       name: this.contactsForm.value.name,
       email: this.contactsForm.value.email,
       phone: this.contactsForm.value.phone,
       favorite: this.contactsForm.value.favorite
+    };
+    
+    if (this.editMode && this.editId) {
+      this.http.put(`http://localhost:5070/api/contacts/${this.editId}`, contactRequest)
+      .subscribe({
+        next: (value) => {
+          console.log(value);
+          this.contacts$ = this.getContacts();
+          this.contactsForm.reset();
+          this.editMode = false;
+          this.editId = null;
+        }
+      });
+    } else {
+      this.http.post('http://localhost:5070/api/contacts', contactRequest)
+      .subscribe({
+        next: (value) => {
+          console.log(value);
+          this.contacts$ = this.getContacts();
+          this.contactsForm.reset();
+        }
+      });
     }
-    this.http.post('http://localhost:5070/api/contacts', addContactRequest)
-    .subscribe({
-      next: (value) => {
-        console.log(value);
-        this.contacts$ = this.getContacts();
-        this.contactsForm.reset();
-      }
-    });
   }
+
+  onEdit(contact: Contact) {
+    this.contactsForm.patchValue({
+      name: contact.name,
+      email: contact.email,
+      phone: contact.phone,
+      favorite: contact.favorite
+    });
+    this.editMode = true;
+    this.editId = contact.id;
+  }
+
   onDelete(id: string) {
     this.http.delete(`http://localhost:5070/api/contacts/${id}`)
     .subscribe({
       next: (value) => {
-        alert('Item deleted')
+        alert('Item deleted');
         this.contacts$ = this.getContacts();
       }
-    })
+    });
   }
 
   private getContacts(): Observable<Contact[]> {
     return this.http.get<Contact[]>('http://localhost:5070/api/contacts');
   }
 }
- 
